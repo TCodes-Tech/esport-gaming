0502f62bacfc244711c775d3d961fc60809e8bad
TCodes-Tech <ethanch1854q@gmail.com>
2026-04-17 16:55:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector("#hamburger")
+const menu = document.querySelector("#menu")
+
+hamburger.addEventListener("click", function() {
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block"
+  }
+)
